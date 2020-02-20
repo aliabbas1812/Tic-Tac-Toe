5bca583b5b00487bb8862c23aae683f14286aec0
Abbas Magar <ali.abbas.magar@accenture.com>
2020-02-20 10:05:17 +0530

    Updates

---
+++ precache-manifest.ff0b14eeee3bdf4ee78049cda52f07bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce490bff1e9fe3a94b4f4bdbd2271c1",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "03dacf121834e7dd6240",
+    "url": "/Tic-Tac-Toe/static/css/main.efb91d30.chunk.css"
+  },
+  {
+    "revision": "ce2bd88395215ea7a362",
+    "url": "/Tic-Tac-Toe/static/js/2.7e3070c1.chunk.js"
+  },
+  {
+    "revision": "497d7e07705261348096eb742962e0f0",
+    "url": "/Tic-Tac-Toe/static/js/2.7e3070c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03dacf121834e7dd6240",
+    "url": "/Tic-Tac-Toe/static/js/main.77262841.chunk.js"
+  },
+  {
+    "revision": "485081d59a3e638c56c1",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.0442fd18.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tic-Tac-Toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
